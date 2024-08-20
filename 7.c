ba4b7db45e35752be82b971dbb2e837aac116407
ShataksheeMishra <shatakshee.mishra@gmail.com>
2024-08-21 00:06:31 +0530

    7.c

--- 7.c
+++ 7.c
@@ -1,3 +1,4 @@
+/*Write a program to copy file1 into file2 ($cp file1 file2).*/
 #include<stdio.h>
 #include<fcntl.h>
 #include<unistd.h>
@@ -9,5 +10,11 @@ return 0;
 }
 int fd_read=open(argf[1], O_RDONLY);
 int fd_write=open(argf[2], O_RDWR | O_CREAT | O_EXCL, 0744);
-
-
+while (1)
+{char buff;
+int r=read(fd_read,&buff,sizeof buff);
+if (r==0)
+break;
+int w=write(fd_write,&buff,sizeof buff);
+}
+}
